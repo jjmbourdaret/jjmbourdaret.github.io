6ea96083e073103f9102dd70c670daec8343ea3c
b <nope@nope.nope>
2021-02-21 19:22:10 +0100

    test

---
+++ yip.js
@@ -0,0 +1,21 @@
+
+
+//import testgj from "/testgj.json.js";
+
+var mymap = L.map('map').setView([45, 6], 10);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+}).addTo(mymap);
+
+var geojson = new L.GeoJSON.AJAX("mp.json");
+
+geojson.on('data:loaded', function(){
+geojson.addTo(mymap);
+});
+
